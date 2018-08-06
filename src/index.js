64844f3e49e45cc56cdbf167d5187fd8774c60d8
Feer Aleksandr <feer@retailsuite.io>
2018-08-07 02:19:19 +0300

    tests 6/6

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@
    createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
  */
 function createDivWithText(text) {
+    let el = document.createElement('div');
+    
+    el.textContent = text;
+    
+    return el;
 }
 
 /*
@@ -22,6 +27,7 @@ function createDivWithText(text) {
    prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
+    where.insertBefore(what, where.firstChild);
 }
 
 /*
@@ -44,6 +50,15 @@ function prepend(what, where) {
    findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
  */
 function findAllPSiblings(where) {
+    let arr = [];
+
+    for (var child of where.children) {
+        if (child.nextElementSibling && child.nextElementSibling.tagName === 'P') {
+            arr.push(child);
+        }
+    }
+
+    return arr;
 }
 
 /*
@@ -66,7 +81,7 @@ function findAllPSiblings(where) {
 function findError(where) {
     var result = [];
 
-    for (var child of where.childNodes) {
+    for (var child of where.children) {
         result.push(child.innerText);
     }
 
@@ -86,6 +101,13 @@ function findError(where) {
    должно быть преобразовано в <div></div><p></p>
  */
 function deleteTextNodes(where) {
+    for (var child of where.childNodes) {
+        if (child.nodeName == "#text") {
+            where.removeChild(child);
+        }
+    }
+
+    return [...where.children].reduce((p, c) => p.outerHTML + c.outerHTML);
 }
 
 /*
